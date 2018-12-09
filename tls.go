02042336cf0f9e388d850094fa2495205bc75c50
Redmaner <jakevdputten@gmail.com>
2018-12-09 17:03:16 +0100

    TLS: Fix TLS client

--- tls.go
+++ tls.go
@@ -83,10 +83,13 @@ func httpCreateTLSConfig(c tlsConfig) *tls.Config {
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		},
-		NextProtos: []string{
+	}
+
+	if c.AutoCert.Enabled {
+		tlsc.NextProtos = []string{
 			"h2", "http/1.1", // enable HTTP/2
 			acme.ALPNProto, // enable tls-alpn ACME challenges
-		},
+		}
 	}
 
 	// Check if the config uses a PrivateCA and load it
@@ -116,7 +119,19 @@ func httpCreateTLSConfig(c tlsConfig) *tls.Config {
 func serverClient(c tlsConfig) *http.Client {
 	if c.Enabled {
 
-		tlsc := httpCreateTLSConfig(c)
+		tlsc := &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			},
+		}
 
 		t := &http.Transport{
 			TLSClientConfig: tlsc,
